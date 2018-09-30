cfb5f6ff2414af2353004955d823d525c490bc73
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-30 13:16:28 -0400

    Initial Commit

---
+++ Assets/_Scripts/Draggable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+    private Vector3 startPosition;
+    private Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class UIManager : MonoBehaviour
+{
+
+    [Header("Panel Settings")]
+    public GameObject panel;
+    public GameObject panelSpawnPoint;
+    public float panelSpeed;
+    public Animator animator;
+
+    [Header("Panel Groups")]
+    public List<Transform> inventoryList;
+    public List<Transform> equippedList;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        panel.SetActive(false);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (panel.activeInHierarchy)
+            {
+                StartCoroutine(this.ClosePanel());
+            }
+            else
+            {
+                panel.SetActive(true);
+                animator.SetInteger("AnimState", 1);
+            }
+        }
+    }
+
+    private IEnumerator ClosePanel()
+    {
+        animator.SetInteger("AnimState", 0);
+        yield return new WaitForSeconds(0.2f);
+        panel.SetActive(false);
+    }
+
+}
